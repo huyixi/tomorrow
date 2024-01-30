856a61088c86b8aa8aa2a80c60b48f357b9fdd35
huyixi <hi@huyixi.com>
2024-01-30 17:58:39 +0800

    feat: add shadow

--- src/store.js
+++ src/store.js
@@ -2,3 +2,4 @@ import { atom } from 'nanostores'
 export const isAboutOpen = atom(false)
 export const isProjectOpen = atom(false)
 export const isContactOpen = atom(false)
+export const isShadowOpen = atom(false)
