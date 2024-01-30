b6c71008f34eef8bfc9521cb6fda6e0bc7b4f470
huyixi <hi@huyixi.com>
2024-01-30 16:29:29 +0800

    feat: add react intergration

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,11 @@
 // astro.config.ts
-import { defineConfig } from 'astro/config'
-import UnoCSS from 'unocss/astro'
+import { defineConfig } from 'astro/config';
+import UnoCSS from 'unocss/astro';
+import react from "@astrojs/react";
 
+// https://astro.build/config
 export default defineConfig({
-  integrations: [
-    UnoCSS({
-      injectReset: true // or a path to the reset file
-    }),
-  ],
-})
+  integrations: [UnoCSS({
+    injectReset: true // or a path to the reset file
+  }), react()]
+});
